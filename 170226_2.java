edd44729472e108166114c4ef7897da56c7a92de
elias-14 <83251877+elias-14@users.noreply.github.com>
2026-02-17 09:57:06 -0700

    Create 170226_2.java

---
+++ 170226_2.java
@@ -0,0 +1,21 @@
+package Pkt1;
+/*
+ * 
+ * Marca error por que  tiene entero no estatico y los metodos piden staticos
+ * 
+ */
+public class enterox {
+	int x;
+	public static int getx() { return x; }
+	public static void setx(int newx) { x = newx;}
+}
+/*
+ * 
+ * Marca error por que tiene statico y los metodos piden no staticos
+ * 
+ */
+public class enterox{
+	static int x;
+	public int getx() { return x; }
+	public void setx(int newx) { x = newx;}
+}
